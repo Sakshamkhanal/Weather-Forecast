e353d1059dd02304e03b43af669e8f91b8cac14b
Saksham Khanal <saksham.khanal01@gmail.com>
2025-07-17 20:25:25 +0545

    Added card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+
+      <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+      </Card>
     </div>
   )
 }
